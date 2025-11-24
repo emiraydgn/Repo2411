4fe4bd80f33185775bbe9be383c1a194f7e686be
emiraydgn <emiraydogan03@gmail.com>
2025-11-24 12:54:21 +0300

    struct Planar

--- task2411.cpp
+++ task2411.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+
+struct Planar {
+     virtual int x() const = 0;
+     virtual int y() const = 0;
+     virtual int abs_sqr() const = 0;
+     virtual ~Planar() = default;
+};
+
+Planar* make(std::istream& is);
+Planar* mostLeft(Planar** pls, size_t k);
+
 int main(){
-    
+    Planar* pts[10] = {};
+     size_t k = 0;
+     pts[k++] = make(std::cin);
 }
